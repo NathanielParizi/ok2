d55004147fa2e18a4a6faf17cde37118cee22413
Nathaniel-Joel Parizi <forexmagnumopus@hushmail.com>
2017-12-01 15:28:58 -0500

    testing

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,10 @@
-print("Testing VCS github in IntelliJ")
-print("テスト１２３")
+greeting = "Hello"
+name = input("please enter your name")
+print(greeting + ' ' + name)
+
+splitString = "This split \n String \n several \n Lines"
+print(splitString)
+
+print("No he's resting")
+print("He's pining \ the parrot is pining")
+print("WAKEYWAKEEEYYYY!!!")
